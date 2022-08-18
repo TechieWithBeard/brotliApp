bfab816ff99558ede2052eec79da1eb558cb3d06
vishnu thankappan <vishnunadaar111@gmail.com>
2022-08-18 11:45:55 +0530

    brotli compression enabled

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,5 +1,7 @@
 'use strict';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const zlib = require("zlib");
+ const CompressionPlugin = require(`compression-webpack-plugin`);
 var path = require('path');
 module.exports = {
 
@@ -10,7 +12,26 @@ module.exports = {
         minimize: true,
     },
     plugins: [
-  
+        new CompressionPlugin({
+            filename: "[path][base].gz",
+            algorithm: "gzip",
+            test: /\.js$|\.css$|\.ttf$|\.woff$|\.woff2$|\.svg$|\.png$/,
+            threshold: 10240,
+            minRatio: 0.8,
+          }),
+          new CompressionPlugin({
+            filename: "[path][base].br",
+            algorithm: "brotliCompress",
+            test: /\.(js|css|svg|ttf|woff|woff2|png)$/,
+            compressionOptions: {
+              params: {
+                [zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY,
+              },
+            },
+            threshold: 10240,
+            minRatio: 0.8,
+          }),
+       
         new HtmlWebpackPlugin({
             template: "./src/index.html",
             filename: "index.html"
